f211a6e253e6e2b95bb90c0f8a8e9582b17827d4
wss534857356 <534857356@qq.com>
2015-10-24 01:04:03 +0800

    更新
    自己刚学到的一些新东西

--- demo/DataAccesslayer.php
+++ demo/DataAccesslayer.php
@@ -5,11 +5,14 @@
  * Date: 2015/10/17
  * Time: 18:40
  */
+include_once("conn.php");
 class LWordDBTask {
 // 添加留言
     public function append($newLWord) {
-
+        session_start();
+        $sql="";
     }
 };
 return 0;
+mysql_close($conn);
 ?>
---
+++ demo/sqllist.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wengshenshun
+ * Date: 2015/10/24
+ * Time: 0:47
+ */
+function inject_check($sql_str) {
+    return eregi('select|insert|and|or|update|delete|\'|\/\*|\*|\.\.\/|\.\/|union|into|load_file|outfile', $sql_str);
+}
+
+function verify_id($id=null) {
+    if(!$id) {
+        exit('没有提交参数！');
+    } elseif(inject_check($id)) {
+        exit('提交的参数非法！');
+    } elseif(!is_numeric($id)) {
+        exit('提交的参数非法！');
+    }
+    $id = intval($id);
+
+    return $id;
+}
+
+
+function str_check( $str ) {
+    if(!get_magic_quotes_gpc()) {
+        $str = addslashes($str); // 进行过滤
+    }
+    $str = str_replace("_", "\_", $str);
+    $str = str_replace("%", "\%", $str);
+
+    return $str;
+}
+
+
+function post_check($post) {
+    if(!get_magic_quotes_gpc()) {
+        $post = addslashes($post);
+    }
+    $post = str_replace("_", "\_", $post);
+    $post = str_replace("%", "\%", $post);
+    $post = nl2br($post);
+    $post = htmlspecialchars($post);
+
+    return $post;
+}
+?>
