971b7921c451bb58c4f0eb99c389c030348a92fe
wss534857356 <534857356@qq.com>
2015-10-25 22:30:29 +0800

    压缩了一下无用的东西
    。。还在继续学习,先把无用的东西剔除掉

--- demo/AppExtension.php
+++ demo/AppExtension.php
@@ -10,9 +10,5 @@ namespace AppExtension;
 
 
 interface AppExtension {
-
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> origin/master
+?>
--- demo/DataAccesslayer.php
+++ demo/DataAccesslayer.php
@@ -10,22 +10,16 @@ class LWordDBTask {
 // 添加留言
     public function append($newLWord) {
         session_start();
-<<<<<<< HEAD
         $userId=$_SESSION['userId'];
-        $query=@mysql_query("insert into message(userId,messageContent)VALUES ('$userId','$newLWord') WHERE NOT exists (select * from userList where userId='$userId') ;");
+        $query=@mysql_query("insert into message(userId,messageContent)VALUES ('$userId','".$newLWord."');");
         if($query)
             return $newLWord."添加成功";
         else
             return "添加失败";
-=======
         $sql="";
->>>>>>> origin/master
     }
 };
 return 0;
 mysql_close($conn);
-<<<<<<< HEAD
+
 ?>
-=======
-?>
->>>>>>> origin/master
